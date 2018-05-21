4a5cd5983486d529e41551cbb9cf63f07e2dd136
walkert <walkert20@hanover.edu>
2018-05-21 19:32:47 -0400

    Updated TopLevelActivity file.

--- app/src/main/java/edu/hanover/starbuzz/TopLevelActivity.java
+++ app/src/main/java/edu/hanover/starbuzz/TopLevelActivity.java
@@ -6,8 +6,18 @@ import android.os.Bundle;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.view.View;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.SimpleCursorAdapter;
+import android.widget.CursorAdapter;
+import android.widget.Toast;
 
 public class TopLevelActivity extends Activity {
+
+    private SQLiteDatabase db;
+    private Cursor favoritesCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +35,37 @@ public class TopLevelActivity extends Activity {
         //Add the listener to the list view
         ListView listView = (ListView) findViewById(R.id.list_options);
         listView.setOnItemClickListener(itemClickListener);
+
+        //Populate the list_favorites ListView from a cursor
+        ListView listFavorites = (ListView)findViewById(R.id.list_favorites);
+        try{
+            SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+            favoritesCursor = db.query("DRINK", new String[] {"_id", "NAME"}, "FAVORITE = 1", null, null, null, null);
+            CursorAdapter favoriteAdapter = new SimpleCursorAdapter(TopLevelActivity.this,
+                    android.R.layout.simple_list_item_1, favoritesCursor, new String[] {"NAME"}, new int[] {android.R.id.text1}, 0);
+            listFavorites.setAdapter(favoriteAdapter);
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        //Navigate to DrinkActivity if a drink is clicked
+        listFavorites.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listView, View v, int position, long id) {
+                Intent intent = new Intent(TopLevelActivity.this, DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int)id);
+                startActivity(intent);
+            }
+        });
+    }
+
+    //Close the cursor and database in the onDestroy() method
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        favoritesCursor.close();
+        db.close();
     }
 }
