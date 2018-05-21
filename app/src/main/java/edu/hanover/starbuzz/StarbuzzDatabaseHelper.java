954199800355a56bf892612f1082e39f7c199250
walkert <walkert20@hanover.edu>
2018-05-21 11:28:43 -0400

    Edited onCreate function and created updateMyDatabase function.

--- app/src/main/java/edu/hanover/starbuzz/StarbuzzDatabaseHelper.java
+++ app/src/main/java/edu/hanover/starbuzz/StarbuzzDatabaseHelper.java
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "starbuzz";
-    private static final int DB_VERSION = 1;
+    private static final int DB_VERSION = 2;
 
     StarbuzzDatabaseHelper (Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -16,7 +16,17 @@ class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE DRINK ("
+        updateMyDatabase(db, 0, DB_VERSION);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase dp, int oldVersion, int newVersion) {
+        updateMyDatabase(db, oldVersion, newVersion);
+    }
+
+    private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion < 1) {
+            db.execSQL("CREATE TABLE DRINK ("
                 + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + "NAME TEXT, "
                 + "DESCRIPTION TEXT, "
@@ -24,11 +34,10 @@ class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
         insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
         insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam", R.drawable.cappuccino);
         insertDrink(db, "Filter", "Our best drip coffee", R.drawable.filter);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase dp, int oldVersion, int newVersion) {
-
+        }
+        if (oldVersion < 2) {
+            db.execSQL("ALTER TABLE DRINK ADD COLUMN FAVORITE NUMERIC;");
+        }
     }
 
     private static void insertDrink(SQLiteDatabase db, String name, String description, int resourceId) {
