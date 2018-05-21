0d2a03586d5c0a6fb8ecf8e6df3c0face616960c
walkert <walkert20@hanover.edu>
2018-05-21 11:20:45 -0400

    Created and defined onCreate and insertDrink functions.

---
+++ app/src/main/java/edu/hanover/starbuzz/StarbuzzDatabaseHelper.java
@@ -0,0 +1,41 @@
+package edu.hanover.starbuzz;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database. sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "starbuzz";
+    private static final int DB_VERSION = 1;
+
+    StarbuzzDatabaseHelper (Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE DRINK ("
+                + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "NAME TEXT, "
+                + "DESCRIPTION TEXT, "
+                + "IMAGE_RESOURCE_ID INTEGER);");
+        insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
+        insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam", R.drawable.cappuccino);
+        insertDrink(db, "Filter", "Our best drip coffee", R.drawable.filter);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase dp, int oldVersion, int newVersion) {
+
+    }
+
+    private static void insertDrink(SQLiteDatabase db, String name, String description, int resourceId) {
+        ContentValues drinkValues = new ContentValues();
+        drinkValues.put("NAME", name);
+        drinkValues.put("DESCRIPTION", description);
+        drinkValues.put("IMAGE_RESOURCE_ID", resourceId);
+        db.insert("DRINK", null, drinkValues);
+    }
+}
